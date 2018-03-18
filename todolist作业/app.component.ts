83a00bc6691b4503b4a068c2000bb98a657cd2dc
yinlinna <703913979@qq.com>
2018-03-18 21:41:24 +0800

    提交todolist作业

---
+++ todolist作业/app.component.ts
@@ -0,0 +1,12 @@
+import { Component,EventEmitter,Input,Output} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  head_data=[];
+  getD
+}
